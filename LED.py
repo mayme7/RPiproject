45aaf0be8d638eed39b2af5ec692bd3f04874f74
mayme7 <83572758+mayme7@users.noreply.github.com>
2021-05-25 00:12:06 +0900

    Update LED.py

--- LED.py
+++ LED.py
@@ -2,8 +2,16 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BOARD)
-LED = 11
+LED = 10
 
-GPIO.setup(LED, GPIO.OUT, initial=GPIO.LOW)
-GPIO.output(LED, GPIO.HIGH)
-time.sleep(10)
+
+def main():
+    GPIO.setup(LED, GPIO.OUT, initial=GPIO.LOW)
+    while 1:
+        GPIO.output(LED, GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(LED, GPIO.LOW)
+        time.sleep(1)
+
+if __name__=='__main__':
+    main()
