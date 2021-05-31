5db58167eef0f67ecdb5ad63dbebc666c7f56199
mayme7 <83572758+mayme7@users.noreply.github.com>
2021-05-31 18:13:54 +0900

    Update and rename GPIO.py to Switch.py

--- Switch.py
+++ Switch.py
@@ -1,20 +1,17 @@
-import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+import RPi.GPIO as GPIO
 import time
 
 LED = 12
 Switch = 10
 
-GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
 
 def main():
-  
-  #Set pin 10 to be an input pin and set initial value to be pulled low (off)
   GPIO.setup(Switch, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
   GPIO.setup(LED, GPIO.OUT)
 
-  while True: 
-    #GPIO.input(Switch)와 GPIO.HIGH를 AND 연산으로 변경 가능
+  while True:
     if GPIO.input(Switch) == GPIO.HIGH:
       print("Button was pushed!")
       GPIO.output(LED, GPIO.HIGH)
