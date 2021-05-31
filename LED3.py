c5dbb3d698f7b91ee55f8638bb218cf313eabcb9
mayme7 <83572758+mayme7@users.noreply.github.com>
2021-05-31 17:05:38 +0900

    Update LED3.py

--- LED3.py
+++ LED3.py
@@ -2,20 +2,22 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BOARD)
-
 LED = 10
 
-GPIO.setup(LED, GPIO.OUT, initial=GPIO.LOW)
+def main():
+    GPIO.setup(LED, GPIO.OUT, initial=GPIO.LOW)
+    try:
+        while 1:
+            GPIO.output(LED, GPIO.HIGH)
+            time.sleep(0.5)
 
-try:
-    while 1:
-        GPIO.output(LED, GPIO.HIGH)
-        time.sleep(0.5)
-        
-        GPIO.output(LED, GPIO.LOW)
-        time.sleep(0.5)
+            GPIO.output(LED, GPIO.LOW)
+            time.sleep(0.5)
 
 except KeyboardInterrupt:
     pass
 
 GPIO.cleanup()
+
+if __name__=='__main__':
+    main()
