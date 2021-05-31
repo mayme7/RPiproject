2174c919dfcab414e0a0aaa71233dfb6d43bad4f
mayme7 <83572758+mayme7@users.noreply.github.com>
2021-05-31 15:13:40 +0900

    Update GPIO.py

--- GPIO.py
+++ GPIO.py
@@ -9,12 +9,12 @@ GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 
 def main():
   
-  # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+  #Set pin 10 to be an input pin and set initial value to be pulled low (off)
   GPIO.setup(Switch, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
   GPIO.setup(LED, GPIO.OUT)
 
   while True: 
-    # GPIO.input(Switch)와 GPIO.HIGH를 AND 연산으로 변경 가능
+    #GPIO.input(Switch)와 GPIO.HIGH를 AND 연산으로 변경 가능
     if GPIO.input(Switch) == GPIO.HIGH:
       print("Button was pushed!")
       GPIO.output(LED, GPIO.HIGH)
