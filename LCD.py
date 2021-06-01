9db938e4cf7bf059d4640defa8e25472949c47d8
mayme7 <83572758+mayme7@users.noreply.github.com>
2021-06-01 22:37:13 +0900

    Create LCD.py

---
+++ LCD.py
@@ -0,0 +1,30 @@
+import RPi.GPIO as GPIO
+import I2C_driver as LCD
+from time import*
+
+Switch= 10
+LED = 12
+mylcd = LCD.lcd()
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(LED, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(Switch, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+while True:
+    if GPIO.input(Switch) == GPIO.HIGH:
+        print("Button was pushed!")
+        GPIO.output(LED, GPIO.HIGH)
+        mylcd.lcd_display_string("LED ON!",1)
+        time.sleep(1)
+        mylcd.lcd_clear()
+        sleep(0.5)
+    else:
+        print("Button was not pushed!")
+        GPIO.output(LED, GPIO.LOW)
+        mylcd.lcd_display_string("LED OFF!",1)
+        time.sleep(1)
+        mylcd.lcd_clear()
+        sleep(0.5)
+        
