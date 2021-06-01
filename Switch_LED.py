0f864281cbe4c7933d86862a30c12bc0ee2a1975
mayme7 <83572758+mayme7@users.noreply.github.com>
2021-06-01 21:19:59 +0900

    Update Switch_LED.py

--- Switch_LED.py
+++ Switch_LED.py
@@ -1,11 +1,11 @@
 import RPi.GPIO as GPIO
 import time
 
-LED = 8
-Switch = 10
+Switch = 15
+LED = 18
 
 GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 
 def main():
   GPIO.setup(Switch, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -15,16 +15,13 @@ def main():
     while True:
       if GPIO.input(Switch) == GPIO.HIGH:
         GPIO.output(LED, GPIO.HIGH)
-        print("LED ON!")
         time.sleep(0.5)
       else:
         GPIO.output(LED, GPIO.LOW)
-        print("LED OFF!")
         time.sleep(0.5)
         
   finally:
     GPIO.cleanup()
    
-
 if __name__ == '__main__':
     main()
