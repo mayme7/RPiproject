1f3aa0ed6ec06cc4980863b7fc18cc652e0a1d36
mayme7 <83572758+mayme7@users.noreply.github.com>
2021-06-11 21:44:48 +0900

    Create LCD_RGB_ON.py

---
+++ LCD_RGB_ON.py
@@ -0,0 +1,41 @@
+import RPi.GPIO as GPIO
+import I2C_driver as LCD
+from time import *
+
+Switch = 10
+LED_G = 12
+LED_B = 16
+LED_R = 18
+mylcd = LCD.lcd()
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(LED, GPIO.OUT, initial=GPIO.LOW)
+
+  try:
+    while 1:
+        print("Green LED ON!")
+        mylcd.lcd_display_string("Green LED ON!",1)
+        GPIO.output(LED_G, GPIO.HIGH)
+        time.sleep(3)
+        GPIO.output(LED_G, GPIO.LOW)
+        time.sleep(0.5)
+        print("Blue LED ON!")
+        mylcd.lcd_display_string("Blue LED ON!",1)
+        GPIO.output(LED_B, GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(LED_B, GPIO.LOW)
+        time.sleep(0.5)
+        print("Red LED ON!")
+        mylcd.lcd_display_string("Red LED ON!",1)
+        GPIO.output(LED_R, GPIO.HIGH)
+        time.sleep(2)
+        GPIO.output(LED_R, GPIO.LOW)
+        time.sleep(0.5)
+        
+  finally:
+    GPIO.cleanup()
+   
+if __name__ == '__main__':
+    main()
