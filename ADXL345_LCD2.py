3188d319c930fee0d089357d45c376d80ba63c9a
mayme7 <83572758+mayme7@users.noreply.github.com>
2021-06-14 21:59:06 +0900

    Update ADXL345_LCD2.py

--- ADXL345_LCD2.py
+++ ADXL345_LCD2.py
@@ -31,7 +31,6 @@ def init_ADXL345():
     print('ADXL345 init function')
     bus.write_byte_data(address, 0x2D, 0x08)
 
-
 # data measure
 def measure_acc(adr):
     acc0 = bus.read_byte_data(address, adr)
@@ -47,78 +46,8 @@ def measure_acc(adr):
 
     return acc
 
-
 # dot matrix
 def demo(n, block_orientation, rotate, inreverse):
-    # create matrix device
-    serial = spi(port=0, device=0, gpio=noop())
-    device = max7219(serial, cascaded=n or 1, block_orientation=block_orientation,
-                     rotate=rotate or 0, blocks_arranged_in_reverse_order=inreverse)
-    print("Created device")
-
-    # start demo
-    msg = "dot matrix OUTPUT!"
-    print(msg)
-    show_message(device, msg, fill="white", font=proportional(CP437_FONT))
-    time.sleep(1)
-
-    show_message(device, msg, fill="white", font=proportional(LCD_FONT), scroll_delay=0)
-    time.sleep(1)
-
-    show_message(device, msg, fill="white", font=proportional(LCD_FONT), scroll_delay=0.1)
-
-    # words= ['0','1','2','3','4','5','6','7','8','9']
-    words = ['O', 'X']
-    virtual = viewport(device, width=device.width, height=len(words) * 8)
-    with canvas(virtual) as draw:
-        for i, word in enumerate(words):
-            text(draw, (0, i * 8), word, fill="white", font=proportional(CP437_FONT))
-            time.sleep(0.1)
-
-    for i in range(virtual.height - device.height):
-        virtual.set_position((0, i))
-        time.sleep(0.1)
-
-    show_message(device, msg, fill="white")
-
-    time.sleep(0.5)
-
-    print('Canvas')
-    for i in words:
-        print(i, type(i))
-        with canvas(device) as draw:
-            # text(draw, (0, 0), "A", fill="white")
-            text(draw, (0, 0), i, fill="white")
-
-        time.sleep(0.1)
-
-        for _ in range(5):
-            for intensity in range(16):
-                device.contrast(intensity * 16)
-                time.sleep(0.5)
-
-    device.contrast(0x80)
-    time.sleep(1)
-
-    show_message(device, msg, fill="white", font=SINCLAIR_FONT)
-
-    time.sleep(1)
-    show_message(device, msg, fill="white", font=proportional(SINCLAIR_FONT))
-
-    time.sleep(1)
-    show_message(device, msg, fill="white", font=proportional(TINY_FONT))
-
-    time.sleep(1)
-    show_message(device, msg)
-
-    time.sleep(1)
-    for x in range(256):
-        with canvas(device) as draw:
-            text(draw, (0, 0), chr(x), fill="white")
-            time.sleep(0.1)
-
-
-def main():
     print(bus)
     init_ADXL345()
     
@@ -128,7 +57,8 @@ def main():
             print("1")
             mylcd.lcd_display_string("LCD OUTPUT1", 1)
             mylcd.lcd_display_string("LCD OUTPUT2", 2)
-
+            mylcd.lcd_clear() 
+            
         elif num == '2':  # 01234 5
             print("2")
             x_acc = measure_acc(x_adr)
@@ -136,21 +66,81 @@ def main():
             z_acc = measure_acc(z_adr)
 
             print('X = %2.2f' % x_acc, '[g], Y = %2.2f' % y_acc, '[g], Z = %2.2f' % z_acc, '[g]')
-            mylcd.lcd_clear()
-
+            mylcd.lcd_clear()    
+            
         elif num == '3':  # LCD Dot Matrix
             print("3")
             mylcd.lcd_display_string("LCD OUTPUT1", 1)
-            mylcd.lcd_display_string("LCD OUTPUT2", 2)
+            mylcd.lcd_display_string("LCD OUTPUT2", 2)    
+            
+            # create matrix device
+            serial = spi(port=0, device=0, gpio=noop())
+            device = max7219(serial, cascaded=n or 1, block_orientation=block_orientation,
+                             rotate=rotate or 0, blocks_arranged_in_reverse_order=inreverse)
+            print("Created device")
 
-    try:
-        demo(args.cascaded, args.block_orientation, args.rotate, args.reverse_order)
-    except KeyboardInterrupt:
-        pass
+            # start demo
+            msg = "dot matrix OUTPUT!"
+            print(msg)
+            show_message(device, msg, fill="white", font=proportional(CP437_FONT))
+            time.sleep(1)
 
+            show_message(device, msg, fill="white", font=proportional(LCD_FONT), scroll_delay=0)
+            time.sleep(1)
+
+            show_message(device, msg, fill="white", font=proportional(LCD_FONT), scroll_delay=0.1)
+
+            # words= ['0','1','2','3','4','5','6','7','8','9']
+            words = ['O', 'X']
+            virtual = viewport(device, width=device.width, height=len(words) * 8)
+            with canvas(virtual) as draw:
+                for i, word in enumerate(words):
+                    text(draw, (0, i * 8), word, fill="white", font=proportional(CP437_FONT))
+                    time.sleep(0.1)
+
+            for i in range(virtual.height - device.height):
+                virtual.set_position((0, i))
+                time.sleep(0.1)
+
+            show_message(device, msg, fill="white")
+
+            time.sleep(0.5)
+
+            print('Canvas')
+            for i in words:
+                print(i, type(i))
+                with canvas(device) as draw:
+                    # text(draw, (0, 0), "A", fill="white")
+                    text(draw, (0, 0), i, fill="white")
+
+                time.sleep(0.1)
+
+                for _ in range(5):
+                    for intensity in range(16):
+                        device.contrast(intensity * 16)
+                        time.sleep(0.5)
+
+            device.contrast(0x80)
+            time.sleep(1)
+
+            show_message(device, msg, fill="white", font=SINCLAIR_FONT)
+
+            time.sleep(1)
+            show_message(device, msg, fill="white", font=proportional(SINCLAIR_FONT))
+
+            time.sleep(1)
+            show_message(device, msg, fill="white", font=proportional(TINY_FONT))
+
+            time.sleep(1)
+            show_message(device, msg)
+
+            time.sleep(1)
+            for x in range(256):
+                with canvas(device) as draw:
+                    text(draw, (0, 0), chr(x), fill="white")
+                    time.sleep(0.1)
 
 if __name__ == '__main__':
-    main()
     parser = argparse.ArgumentParser(description='matrix_demo arguments',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
